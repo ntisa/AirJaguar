37e89f9dbd6f3f25a696458dbf07241d1a1c51a3
Akop Karapetyan <dev@akop.org>
2013-12-08 13:45:21 -0800

    Added tiles, sprites and objects

---
+++ src/org/akop/airjag/GameView.java
@@ -0,0 +1,11 @@
+package org.akop.airjag;
+
+import android.content.Context;
+import android.view.SurfaceView;
+
+public class GameView extends SurfaceView {
+
+	public GameView(Context context) {
+		super(context);
+	}
+}
